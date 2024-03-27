9b08803b03a5079f2e7b2da41759ee5c272a84d4
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-03-27 11:02:43 +0100

    - Reldens - Tile Map Generator

--- examples/layer-elements-composite/generate-multiples-with-associations.js
+++ examples/layer-elements-composite/generate-multiples-with-associations.js
@@ -11,7 +11,7 @@ const rootFolder = __dirname;
 const execute = async () => {
     let generators = {};
     for(let mapFileName of ['map-associations-001', 'map-associations-002', 'map-associations-003']){
-        generators[mapFileName] = await RandomMapGenerator.fromComposite({
+        let generationOptions = {
             tileMapJSON: JSON.parse(JSON.stringify(tileMapJSON)),
             mapFileName,
             rootFolder,
@@ -19,8 +19,20 @@ const execute = async () => {
             blockMapBorder: true,
             freeSpaceTilesQuantity: 2,
             variableTilesPercentage: 15,
-            collisionLayersForPaths: ['change-points', 'collisions']
-        });
+            collisionLayersForPaths: ['change-points', 'collisions'],
+            writeCroppedElementsFiles: true,
+            associatedMapsConfig: {
+                generateElementsPath: false,
+                blockMapBorder: true,
+                freeSpaceTilesQuantity: 1,
+                variableTilesPercentage: 0,
+                placeElementsOrder: 'inOrder',
+                orderElementsBySize: false,
+                randomizeQuantities: true,
+                writeCroppedElementsFiles: true
+            }
+        };
+        generators[mapFileName] = await RandomMapGenerator.fromComposite(generationOptions);
         await generators[mapFileName].generate();
     }
 };
