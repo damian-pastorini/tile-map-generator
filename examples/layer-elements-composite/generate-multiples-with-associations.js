ad5a2ce6ee51d53d0de7ee0c94df1b95fb6dc099
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-03-28 20:38:11 +0100

    - Reldens - Tile Map Generator

--- examples/layer-elements-composite/generate-multiples-with-associations.js
+++ examples/layer-elements-composite/generate-multiples-with-associations.js
@@ -10,7 +10,15 @@ const rootFolder = __dirname;
 
 const execute = async () => {
     let generators = {};
-    for(let mapFileName of ['map-associations-001', 'map-associations-002', 'map-associations-003']){
+    let generatedMaps = {};
+    let mapNames = ['map-associations-001', 'map-associations-002', 'map-associations-003'];
+    let i = 0;
+    for(let mapFileName of mapNames){
+        let previousGenerator = 0 < i ? generators[mapNames[i - 1]] : null;
+        let previousMainPath = [];
+        if (previousGenerator) {
+            previousMainPath = !previousGenerator.hasAssociatedMap ? previousGenerator.generatedMainPathIndexes : [];
+        }
         let generationOptions = {
             tileMapJSON: JSON.parse(JSON.stringify(tileMapJSON)),
             mapFileName,
@@ -21,20 +29,22 @@ const execute = async () => {
             variableTilesPercentage: 15,
             collisionLayersForPaths: ['change-points', 'collisions'],
             // writeCroppedElementsFiles: true,
+            previousMainPath,
             associatedMapsConfig: {
                 generateElementsPath: false,
                 blockMapBorder: true,
-                freeSpaceTilesQuantity: 2,
+                freeSpaceTilesQuantity: 1,
                 variableTilesPercentage: 0,
-                placeElementsOrder: 'inOrder',
-                orderElementsBySize: false,
-                randomizeQuantities: true,
+                placeElementsOrder: 'inOrder', // this will place the elements one beside the other in the generated map
+                orderElementsBySize: true, // this will order the elements by size
+                randomizeQuantities: false, // when ordering the elements by size we need to set this "false"
                 applySurroundingPathTiles: false
                 // writeCroppedElementsFiles: true
             }
         };
         generators[mapFileName] = await RandomMapGenerator.fromComposite(generationOptions);
-        await generators[mapFileName].generate();
+        generatedMaps[mapFileName] = await generators[mapFileName].generate();
+        i++;
     }
 };
 
