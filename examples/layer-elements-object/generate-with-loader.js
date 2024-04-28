6a44aa46f4814acd8ebe38a4d4c3a46beeb771ec
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-04-28 16:19:37 +0200

    - Reldens - Tile Map Generator

--- examples/layer-elements-object/generate-with-loader.js
+++ examples/layer-elements-object/generate-with-loader.js
@@ -10,12 +10,7 @@ const { RandomMapGenerator } = require('../../lib/random-map-generator');
 const execute = async () => {
     const loader = new LayerElementsObjectLoader({
         rootFolder: __dirname,
-        mapDataFile: 'map-data.json',
-        layerElementsFiles: {
-            house1: 'house-001.json',
-            house2: 'house-002.json',
-            tree: 'tree.json'
-        }
+        mapDataFile: 'map-data.json'
     });
     await loader.load();
     const generator = new RandomMapGenerator(loader.mapData);
